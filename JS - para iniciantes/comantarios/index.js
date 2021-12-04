9c3219c3dc976c6454f8864fb110efe4abee7743
unknown <irineu.joel01@gmail.com>
2021-12-04 14:02:46 -0300

    comatarios

---
+++ JS - para iniciantes/comantarios/index.js
@@ -0,0 +1,13 @@
+// COmentarios são trechos ignorados no momento de executar o codigo
+console.log('Olá, mundo'); // Aqui tem outro comentario.
+console.log('linha 1');
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+// Vai ser ignorado
+/**
+ * Comentario
+ * de
+ * multiplas
+ * linhas
+ */
