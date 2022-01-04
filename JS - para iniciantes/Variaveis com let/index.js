6ec6f26e1d7ef35e8a7eef77743c0e3a4f339f96
unknown <irineu.joel01@gmail.com>
2022-01-04 09:28:36 -0300

    variaveis com let

---
+++ JS - para iniciantes/Variaveis com let/index.js
@@ -0,0 +1,16 @@
+let nome = 'João'; //String
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve um filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+
+// Não podemos criar variaveis com palavras reservadas
+// variaveis precisam ter nomes significativos
+// Não pode começar variavel com numero
+// não pode conter espaço ou traços
+// utilizamos camelCase
+// Case-sensitive
+// não podemos redelcarar variaveis com let
+// NÂO UTILIZE VAR, UTILIZE LET
