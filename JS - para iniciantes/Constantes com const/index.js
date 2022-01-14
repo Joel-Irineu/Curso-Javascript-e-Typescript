4827dc6f2d99b1010762a66b01f52b6ae550ca84
unknown <irineu.joel01@gmail.com>
2022-01-14 09:50:36 -0300

    constantes com const

---
+++ JS - para iniciantes/Constantes com const/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// Não pode começar constante com numero
+// não pode conter espaço ou traços
+// utilizamos camelCase
+// Case-sensitive
+// não podemos redelcarar constantes ou modificar seu valor
+// NÂO UTILIZE VAR, UTILIZE CONST
+// Sempre colocar um valor já na criação da contante
+
+const nome = 'Joel';
+let   Nome = 'joel';
+console.log(nome);
