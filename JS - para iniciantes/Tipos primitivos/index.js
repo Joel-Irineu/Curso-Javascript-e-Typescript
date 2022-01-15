7ca142b2d2b878e9b830887b992a4247c97fb139
unknown <irineu.joel01@gmail.com>
2022-01-15 14:41:08 -0300

    tipos primitivos

---
+++ JS - para iniciantes/Tipos primitivos/index.js
@@ -0,0 +1,7 @@
+const nome = 'irineu'; //string
+const num = 10; // number
+let nomeAluno; // undefined
+const sobrenome = null; //nulo
+const aprovado = true; //bolean -> verdadeiro ou falso
+
+console.log(typeof aprovado, aprovado);
